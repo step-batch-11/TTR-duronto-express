6197546f04efeb8dccfba4fb507805f1308ca308
Ayush Verma <vermajiii8116@gmail.com>
2026-03-27 16:33:36 +0530

    [#<story number>] | <commit message> | <author>/<pair>

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 cp setup/hooks/* .git/hooks
 
 chmod +x .git/hooks/pre-commit
-chmod +x .git/hooks/pre-push
+chmod +x .git/hooks/prepare-commit-msg
 
 deno add jsr:@std/assert
 deno add jsr:@std/testing
