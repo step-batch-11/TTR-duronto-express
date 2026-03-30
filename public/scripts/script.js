fa695b3390887a77343143e577ae26a671aae588
Mohammed Haji Hussain <mohammedhajihussain@Mohammeds-MacBook-Pro.local>
2026-03-30 17:00:00 +0530

    [#4] | fixes the lint issue | haji/deepak

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,4 +1,4 @@
-const fetchPlayerDetails = async () => {
+const fetchPlayerDetails = () => {
   return [
     {
       name: "a",
@@ -16,43 +16,46 @@ const fetchPlayerDetails = async () => {
       carCount: 45,
     },
   ];
-}
+};
 
 const appendPlayer = ({ name, symbol, carCount }, container, template) => {
   const clone = template.content.cloneNode(true);
-  clone.querySelector('.identifier .name').textContent = name;
-  clone.querySelector('.identifier .symbol').style.backgroundColor = symbol;
+  clone.querySelector(".identifier .name").textContent = name;
+  clone.querySelector(".identifier .symbol").style.backgroundColor = symbol;
 
-  clone.querySelector('.train-car-data img').setAttribute('src', '/something');
-  clone.querySelector('.train-car-data .car-count').textContent = carCount;
+  clone.querySelector(".train-car-data img").setAttribute("src", "/something");
+  clone.querySelector(".train-car-data .car-count").textContent = carCount;
 
   container.append(clone);
-}
+};
 
 const displayPlayers = (players) => {
-  const playerTemplate = document.querySelector('#user');
-  const container = document.querySelector('.player-details');
+  const playerTemplate = document.querySelector("#user");
+  const container = document.querySelector(".player-details");
   players.forEach((player) => {
     appendPlayer(player, container, playerTemplate);
   });
-}
+};
 
-const fetchFaceUpCards = async () => {
+const fetchFaceUpCards = () => {
   return ["red", "green", "wild", "yellow", "blue"];
-}
+};
 
 const displayFaceUpCards = (cards) => {
   const cardTemplate = document.querySelector("#face-up-cards");
   const container = document.querySelector(".faceup-cards");
   cards.forEach((card, index) => {
     const clone = cardTemplate.content.cloneNode(true);
-    clone.querySelector('.card').id = index;
-    clone.querySelector('.card img').setAttribute('src', `./assets/car-cards-images/${card}.jpg`);
+    clone.querySelector(".card").id = index;
+    clone.querySelector(".card img").setAttribute(
+      "src",
+      `./assets/car-cards-images/${card}.jpg`,
+    );
     container.append(clone);
   });
-}
+};
 
 globalThis.onload = () => {
   fetchPlayerDetails().then(displayPlayers);
   fetchFaceUpCards().then(displayFaceUpCards);
-}
+};
