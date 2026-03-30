e1b1179539e86ccb826a52f652e49fd684c8047e
Mohammed Haji Hussain <mohammedhajihussain@Mohammeds-MacBook-Pro.local>
2026-03-30 16:55:25 +0530

    [#4] | mocks the data to send faceup cards and player data to frontend | haji/deepak

---
+++ public/scripts/script.js
@@ -0,0 +1,58 @@
+const fetchPlayerDetails = async () => {
+  return [
+    {
+      name: "a",
+      symbol: "green",
+      carCount: 45,
+    },
+    {
+      name: "b",
+      symbol: "red",
+      carCount: 45,
+    },
+    {
+      name: "v",
+      symbol: "blue",
+      carCount: 45,
+    },
+  ];
+}
+
+const appendPlayer = ({ name, symbol, carCount }, container, template) => {
+  const clone = template.content.cloneNode(true);
+  clone.querySelector('.identifier .name').textContent = name;
+  clone.querySelector('.identifier .symbol').style.backgroundColor = symbol;
+
+  clone.querySelector('.train-car-data img').setAttribute('src', '/something');
+  clone.querySelector('.train-car-data .car-count').textContent = carCount;
+
+  container.append(clone);
+}
+
+const displayPlayers = (players) => {
+  const playerTemplate = document.querySelector('#user');
+  const container = document.querySelector('.player-details');
+  players.forEach((player) => {
+    appendPlayer(player, container, playerTemplate);
+  });
+}
+
+const fetchFaceUpCards = async () => {
+  return ["red", "green", "wild", "yellow", "blue"];
+}
+
+const displayFaceUpCards = (cards) => {
+  const cardTemplate = document.querySelector("#face-up-cards");
+  const container = document.querySelector(".faceup-cards");
+  cards.forEach((card, index) => {
+    const clone = cardTemplate.content.cloneNode(true);
+    clone.querySelector('.card').id = index;
+    clone.querySelector('.card img').setAttribute('src', `./assets/car-cards-images/${card}.jpg`);
+    container.append(clone);
+  });
+}
+
+globalThis.onload = () => {
+  fetchPlayerDetails().then(displayPlayers);
+  fetchFaceUpCards().then(displayFaceUpCards);
+}
